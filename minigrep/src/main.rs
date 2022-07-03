5b7195e803e6e58bf0acb08d6184508c2469faa2
Jared Dunn <jdunn@webucator.com>
2022-07-03 18:04:11 -0400

    feat: add Config struct and use instead of tuple

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,18 +4,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {} in {}", query, filename);
+    println!("Searching for {} in {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
