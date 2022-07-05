c9a54facf1793ce34900c2f672fddb51fb0afe48
Jared Dunn <jdunn@webucator.com>
2022-07-04 21:49:10 -0400

    feat: print err for config error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,9 +7,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
         process::exit(1);
     });
-    if let Err(e) = minigrep::run(config) {
+    if let Err(_e) = minigrep::run(config) {
         process::exit(1);
     }
 }
