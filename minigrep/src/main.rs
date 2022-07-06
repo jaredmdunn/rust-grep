7894eb4b7b089bd9f9ee8594452946305ecb0d94
Jared Dunn <jdunn@webucator.com>
2022-07-05 20:55:28 -0400

    feat: use eprintln to send output to file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,10 +7,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    if let Err(_e) = minigrep::run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
